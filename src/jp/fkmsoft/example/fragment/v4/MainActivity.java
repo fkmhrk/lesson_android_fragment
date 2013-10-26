fd385e6686eef6e9460f0d77b07ee85aec95bb0a
hrk fkm <fkm@fkmsoft.jp>
2013-10-26 22:02:16 +0900

    Add login fragment

--- src/jp/fkmsoft/example/fragment/v4/MainActivity.java
+++ src/jp/fkmsoft/example/fragment/v4/MainActivity.java
@@ -1,22 +1,25 @@
 package jp.fkmsoft.example.fragment.v4;
 
+import jp.fkmsoft.example.fragment.v4.page.login.LoginFragment;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        if (savedInstanceState == null) {
+            FragmentManager manager = getSupportFragmentManager();
+            FragmentTransaction transaction = manager.beginTransaction();
+            
+            transaction.replace(R.id.main, LoginFragment.newInstance());
+            
+            transaction.commit();
+        }
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
 }
---
+++ src/jp/fkmsoft/example/fragment/v4/page/login/LoginFragment.java
@@ -0,0 +1,25 @@
+package jp.fkmsoft.example.fragment.v4.page.login;
+
+import jp.fkmsoft.example.fragment.v4.R;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LoginFragment extends Fragment {
+    public static LoginFragment newInstance() {
+        LoginFragment fragment = new LoginFragment();
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_login, container, false);
+        
+        return root;
+    }
+    
+    
+}
